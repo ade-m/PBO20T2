58c4f7ad2f8532a62b100a5aa25cc6dd8714e6ff
ade-m <66313509+ade-m@users.noreply.github.com>
2021-06-03 18:16:53 +0700

    pertemuan 3

---
+++ Pertemuan3/pertemuan3-loop/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // for(int i=1;i<=10;i++){
+        //     System.out.println("i="+i);
+        // }
+
+        //array
+        int[] bilangandesimal = {3,4,5,6,7,8,9,10,11,12,13};
+        for(int i=0;i<9;i++){
+            System.out.println("Bilangan ke-"+i+"="+ bilangandesimal[i]);
+        }
+        
+        for(int i:bilangandesimal){
+            System.out.print(i+",");
+        }
+
+        for(int i=0;i<bilangandesimal.length;i++){
+            System.out.println("Bilangan ke-"+i+"="+ bilangandesimal[i]);
+        }
+
+    }
+}
