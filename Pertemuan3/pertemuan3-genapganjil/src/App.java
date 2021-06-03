4859cff3cf7e2b89135a547e20c8a70270ce90aa
ade-m <66313509+ade-m@users.noreply.github.com>
2021-06-03 18:48:10 +0700

    genap ganjil

---
+++ Pertemuan3/pertemuan3-genapganjil/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int n=9;
+        for(int i =1;i<=n;i++){
+            //jika genap adalah bilangan yang habis jika dibagi 2 maka cetak
+            for(int j=1;j<=i;j++){
+            if(j%2==0)
+                System.out.print(j+",");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
