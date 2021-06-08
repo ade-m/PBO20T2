a9d4b69b5b5a4aba8c10aef144ac9e72abb351eb
ade-m <66313509+ade-m@users.noreply.github.com>
2021-06-08 22:35:19 +0700

    quiz kelas

--- Pertemuna4/QUIZKELAS1/kuisKelas1/src/App.java
+++ Pertemuna4/QUIZKELAS1/kuisKelas1/src/App.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-       String kata ="UPHMEDAN20T2";
+       String kata ="UPHMEDAN20TI2";
        int n=5;
+       String yn="y";
+
+       Scanner masukan = new Scanner(System.in);
+       
+       do{
+       
+        System.out.print("Input N :");
+        n= masukan.nextInt();
+        if(n>kata.length()){
+            System.out.println("Error n > panjang kata");
+        }
+        else{
+             for(int i=0;i<=n-1;i++){
+                 for(int j=0;j<=i;j++)
+                 {System.out.print(kata.substring(j, j+1));}
+                 System.out.println("");
+             }
+     
+             for(int i=n-2;i>=0;i--){
+                 for(int j=0;j<=i;j++)
+                     {System.out.print(kata.substring(j, j+1));}
+                 System.out.println("");
+             }
+ 
+        }    
+
+        System.out.print("Lanjut? (Y/N)");
+        masukan.nextLine();
+        yn= masukan.nextLine();
+       }while(yn.equals("y"));
+       
        
     }
 }
